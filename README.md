00991de90e137b34d651e90c7b7d1bd3b542f48a
PierreGode <pierre@gode.one>
2024-09-03 13:14:48 +0200

    deploy
    deploy

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# GPTcode-reviwer
-AI GPTcode-reviwer
+# Open AI Code Reviewer
+
+[![Known Vulnerabilities](https://snyk.io/test/github/PierreGode/GPTcode-reviwer/badge.svg)](https://snyk.io/test/github/PierreGode/GPTcode-reviwer) [![GitHub issues](https://img.shields.io/github/issues/PierreGode/GPTcode-reviwer)](https://github.com/PierreGode/GPTcode-reviwer/issues) [![GitHub pull requests](https://img.shields.io/github/issues-pr/PierreGode/GPTcode-reviwer)](https://github.com/PierreGode/GPTcode-reviwer/pulls) [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/PierreGode/GPTcode-reviwer/blob/main/LICENSE)
+
+
+AI Code Reviewer is a GitHub Action that leverages OpenAI's GPT-4o-mini API to provide intelligent feedback and suggestions on
+your pull requests. This powerful tool helps improve code quality and saves developers time by automating the code
+review process.
+
+## Features
+
+- Reviews pull requests using OpenAI's GPT-4o API.
+- Provides intelligent comments and suggestions for improving your code.
+- Filters out files that match specified exclude patterns.
+- Easy to set up and integrate into your GitHub workflow.
+
+## Setup
+
+1. To use this GitHub Action, you need an OpenAI API key. If you don't have one, sign up for an API key
+   at [OpenAI](https://beta.openai.com/signup).
+
+2. Add the OpenAI API key as a GitHub Secret in your repository with the name `OPENAI_API_KEY`. You can find more
+   information about GitHub Secrets [here](https://docs.github.com/en/actions/reference/encrypted-secrets).
+
+3. Create a `.github/workflows/main.yml` file in your repository and add the following content:
+
+```yaml
+name: AI Code Reviewer
+
+on:
+  pull_request:
+    types:
+      - opened
+      - synchronize
+permissions: write-all
+jobs:
+  review:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Repo
+        uses: actions/checkout@v3
+
+      - name: AI Code Reviewer
+        uses: your-username/ai-code-reviewer@main
+        with:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          OPENAI_API_KEY: ${{ secrets.OPENAI_API_KEY }}
+          OPENAI_API_MODEL: "gpt-4o-mini" # Optional: defaults to "gpt-4o-mini"
+          REVIEW_MAX_COMMENTS: 5 # Optional: defaults to 10
+          REVIEW_PROJECT_CONTEXT: "PHP 8.3 + Laravel 10 + PHPUnit 7." # Optional
+          exclude: "**/*.json, **/*.md" # Optional: exclude patterns separated by commas
+```
+
+4. Replace `your-username` with your GitHub username or organization name where the AI Code Reviewer repository is
+   located.
+
+5. Customize the `exclude` input if you want to ignore certain file patterns from being reviewed.
+
+6. Commit the changes to your repository, and AI Code Reviewer will start working on your future pull requests.
+
+## How It Works
+
+The AI Code Reviewer GitHub Action retrieves the pull request diff, filters out excluded files, and sends code chunks to
+the OpenAI API. It then generates review comments based on the AI's response and adds them to the pull request.
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit issues or pull requests to improve the AI Code Reviewer GitHub
+Action.
+
+## License
+This is a re-builded version from the abandoned ai-codereviewer
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
---
+++ src/main.ts
@@ -0,0 +1,289 @@
+import { readFileSync } from "fs";
+import * as core from "@actions/core";
+import { Configuration, OpenAIApi } from "openai";
+import { Octokit } from "@octokit/rest";
+import parseDiff, { Chunk, File } from "parse-diff";
+import minimatch from "minimatch";
+
+const GITHUB_TOKEN: string = core.getInput("GITHUB_TOKEN");
+const OPENAI_API_KEY: string = core.getInput("OPENAI_API_KEY");
+const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
+const REVIEW_MAX_COMMENTS: string = core.getInput("REVIEW_MAX_COMMENTS");
+const REVIEW_PROJECT_CONTEXT: string = core.getInput("REVIEW_PROJECT_CONTEXT");
+
+const RESPONSE_TOKENS = 1024;
+
+const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+interface PRDetails {
+  owner: string;
+  repo: string;
+  pull_number: number;
+  title: string;
+  description: string;
+}
+
+interface AICommentResponse {
+  file: string;
+  lineNumber: string;
+  reviewComment: string;
+}
+
+interface GithubComment {
+  body: string;
+  path: string;
+  line: number;
+}
+
+async function getPRDetails(): Promise<PRDetails> {
+  const { repository, number } = JSON.parse(
+    readFileSync(process.env.GITHUB_EVENT_PATH || "", "utf8")
+  );
+  const prResponse = await octokit.pulls.get({
+    owner: repository.owner.login,
+    repo: repository.name,
+    pull_number: number,
+  });
+  return {
+    owner: repository.owner.login,
+    repo: repository.name,
+    pull_number: number,
+    title: prResponse.data.title ?? "",
+    description: prResponse.data.body ?? "",
+  };
+}
+
+async function getDiff(
+  owner: string,
+  repo: string,
+  pull_number: number
+): Promise<string | null> {
+  const response = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number,
+    mediaType: { format: "diff" },
+  });
+  // @ts-expect-error - response.data is a string
+  return response.data;
+}
+
+async function analyzeCode(
+  changedFiles: File[],
+  prDetails: PRDetails
+): Promise<Array<GithubComment>> {
+  const prompt = createPrompt(changedFiles, prDetails);
+  const aiResponse = await getAIResponse(prompt);
+
+  const comments: Array<GithubComment> = [];
+
+  if (aiResponse) {
+    const newComments = createComments(changedFiles, aiResponse);
+
+    if (newComments) {
+      comments.push(...newComments);
+    }
+  }
+
+  return comments;
+}
+
+function createPrompt(changedFiles: File[], prDetails: PRDetails): string {
+  const problemOutline = `Your task is to review pull requests (PR). Instructions:
+- Provide the response in following JSON format:  [{"file": <file name>,  "lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
+- DO NOT give positive comments or compliments.
+- DO NOT give advice on renaming variable names or writing more descriptive variables.
+- Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
+- Provide at most ${REVIEW_MAX_COMMENTS} comments. It's up to you how to decide which comments to include.
+- Write the comment in GitHub Markdown format.
+- Use the given description only for the overall context and only comment the code.
+${
+  REVIEW_PROJECT_CONTEXT
+    ? `- Additional context regarding this PR's project: ${REVIEW_PROJECT_CONTEXT}`
+    : ""
+}
+- IMPORTANT: NEVER suggest adding comments to the code.
+- IMPORTANT: Evaluate the entire diff in the PR before adding any comments.
+
+Pull request title: ${prDetails.title}
+Pull request description:
+
+---
+${prDetails.description}
+---
+
+TAKE A DEEP BREATH AND WORK ON THIS THIS PROBLEM STEP-BY-STEP.
+`;
+
+  const diffChunksPrompt = new Array();
+
+  for (const file of changedFiles) {
+    if (file.to === "/dev/null") continue; // Ignore deleted files
+    for (const chunk of file.chunks) {
+      diffChunksPrompt.push(createPromptForDiffChunk(file, chunk));
+    }
+  }
+
+  return `${problemOutline}\n ${diffChunksPrompt.join("\n")}`;
+}
+
+function createPromptForDiffChunk(file: File, chunk: Chunk): string {
+  return `\n
+  Review the following code diff in the file "${file.to}". Git diff to review:
+
+  \`\`\`diff
+  ${chunk.content}
+  ${chunk.changes
+    // @ts-expect-error - ln and ln2 exists where needed
+    .map((c) => `${c.ln ? c.ln : c.ln2} ${c.content}`)
+    .join("\n")}
+  \`\`\`
+  `;
+}
+
+async function getAIResponse(
+  prompt: string
+): Promise<Array<AICommentResponse> | null> {
+  const queryConfig = {
+    model: OPENAI_API_MODEL,
+    temperature: 0.2,
+    max_tokens: RESPONSE_TOKENS,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
+
+  try {
+    const response = await openai.createChatCompletion({
+      ...queryConfig,
+      messages: [
+        {
+          role: "system",
+          content: prompt,
+        },
+      ],
+    });
+
+    const res = response.data.choices[0].message?.content?.trim() || "[]";
+    return JSON.parse(res);
+  } catch (error: any) {
+    console.error("Error Message:", error?.message || error);
+
+    if (error?.response) {
+      console.error("Response Data:", error.response.data);
+      console.error("Response Status:", error.response.status);
+      console.error("Response Headers:", error.response.headers);
+    }
+
+    if (error?.config) {
+      console.error("Config:", error.config);
+    }
+
+    return null;
+  }
+}
+
+function createComments(
+  changedFiles: File[],
+  aiResponses: Array<AICommentResponse>
+): Array<GithubComment> {
+  return aiResponses
+    .flatMap((aiResponse) => {
+      const file = changedFiles.find((file) => file.to === aiResponse.file);
+
+      return {
+        body: aiResponse.reviewComment,
+        path: file?.to ?? "",
+        line: Number(aiResponse.lineNumber),
+      };
+    })
+    .filter((comments) => comments.path !== "");
+}
+
+async function createReviewComment(
+  owner: string,
+  repo: string,
+  pull_number: number,
+  comments: Array<GithubComment>
+): Promise<void> {
+  await octokit.pulls.createReview({
+    owner,
+    repo,
+    pull_number,
+    comments,
+    event: "COMMENT",
+  });
+}
+
+async function main() {
+  const prDetails = await getPRDetails();
+  let diff: string | null;
+  const eventData = JSON.parse(
+    readFileSync(process.env.GITHUB_EVENT_PATH ?? "", "utf8")
+  );
+
+  if (eventData.action === "opened") {
+    diff = await getDiff(
+      prDetails.owner,
+      prDetails.repo,
+      prDetails.pull_number
+    );
+  } else if (eventData.action === "synchronize") {
+    const newBaseSha = eventData.before;
+    const newHeadSha = eventData.after;
+
+    const response = await octokit.repos.compareCommits({
+      headers: {
+        accept: "application/vnd.github.v3.diff",
+      },
+      owner: prDetails.owner,
+      repo: prDetails.repo,
+      base: newBaseSha,
+      head: newHeadSha,
+    });
+
+    diff = String(response.data);
+  } else {
+    console.log("Unsupported event:", process.env.GITHUB_EVENT_NAME);
+    return;
+  }
+
+  if (!diff) {
+    console.log("No diff found");
+    return;
+  }
+
+  const changedFiles = parseDiff(diff);
+
+  const excludePatterns = core
+    .getInput("exclude")
+    .split(",")
+    .map((s) => s.trim());
+
+  const filteredDiff = changedFiles.filter((file) => {
+    return !excludePatterns.some((pattern) =>
+      minimatch(file.to ?? "", pattern)
+    );
+  });
+
+  const comments = await analyzeCode(filteredDiff, prDetails);
+  if (comments.length > 0) {
+    await createReviewComment(
+      prDetails.owner,
+      prDetails.repo,
+      prDetails.pull_number,
+      comments
+    );
+  }
+}
+
+main().catch((error) => {
+  console.error("Error:", error);
+  process.exit(1);
+});
