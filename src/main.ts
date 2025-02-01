0a8600dfad3a4cf8be45d4dad75929e78b1ff435
PierreGode <pierre@gode.one>
2025-02-01 10:44:43 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -142,7 +142,7 @@ function createPromptForDiffChunk(file: File, chunk: Chunk): string {
   ${chunk.content}
   ${chunk.changes
     // @ts-expect-error - ln and ln2 exists where needed
-    .map((c) => `${c.ln ? c.ln : c.ln2} ${c.content}`)
+    .map((c) => `${c.type === 'add' ? '+' : c.type === 'del' ? '-' : ' '} ${c.ln ? c.ln : c.ln2} ${c.content}`)
     .join("\n")}
   \`\`\`
   `;
